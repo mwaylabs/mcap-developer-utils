c5adbff974e891e2e38c53295dc211ac7621d56b
DrMabuse23 <p.brewing@mwaysolutions.com>
2014-12-18 17:16:55 +0100

    fix missing dependency mcap.log

--- symlink.sh
+++ symlink.sh
@@ -16,16 +16,25 @@ rm -rf ./mct-core/node_modules/mcap-log
 rm -rf ./mct-core/node_modules/mcap-logger
 
 # Create dep. symlinks
+ln -s ../../mcap-log ./mcap-cli/node_modules/mcap-log
 ln -s ../../mcap-log ./generator-m-server/node_modules/mcap-log
 ln -s ../../mcap-log ./generator-mcap/node_modules/mcap-log
-ln -s ../../mct-core ./mcap-cli/node_modules/mct-core
-ln -s ../../mcaprc ./mcap-cli/node_modules/mcaprc
-ln -s ../../mcap-log ./mcap-cli/node_modules/mcap-log
-ln -s ../../mcap-application-validation ./mcap-deploy/node_modules/mcap-application-validation
-ln -s ../../mcap-log ./mcap-application-validation/node_modules/mcap-log
-ln -s ../../generator-m ./mct-core/node_modules/generator-m
-ln -s ../../generator-m-server ./mct-core/node_modules/generator-m-server
-ln -s ../../generator-mcap ./mct-core/node_modules/generator-mcap
-ln -s ../../mcap-deploy ./mct-core/node_modules/mcap-deploy
+ln -s ../../mcap-log ./mcap-deploy/node_modules/mcap-log
 ln -s ../../mcap-log ./mct-core/node_modules/mcap-log
+ln -s ../../mcap-log ./mcap-application-validation/node_modules/mcap-log
+
+ln -s ../../mct-core ./mcap-cli/node_modules/mct-core
+
+ln -s ../../mcaprc ./mcap-cli/node_modules/mcaprc
+
+ln -s ../../mcap-application-validation ./mcap-deploy/node_modules/mcap-application-validation
+
+ln -s ../../generator-m ./mct-core/node_modules/generator-m
+
+ln -s ../../generator-m-server ./mct-core/node_modules/generator-m-server
+
+ln -s ../../generator-mcap ./mct-core/node_modules/generator-mcap
+
+ln -s ../../mcap-deploy ./mct-core/node_modules/mcap-deploy
+
 ln -s ../../mcap-logger ./mct-core/node_modules/mcap-logger
