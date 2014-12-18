1637f154098ee5b5eed188138c39423ae360b289
Stefan Buck <github@stefanbuck.com>
2014-12-18 16:03:05 +0100

    replace symlinks

--- cli.sh
+++ cli.sh
@@ -1,9 +1,23 @@
-git clone git@github.com:mwaylabs/generator-m-server.git
-git clone git@github.com:mwaylabs/generator-m.git
-git clone git@github.com:mwaylabs/generator-mcap.git
-git clone git@github.com:mwaylabs/mcap-application-validation.git
-git clone git@github.com:mwaylabs/mcap-cli.git
-git clone git@github.com:mwaylabs/mcap-deploy.git
-git clone git@github.com:mwaylabs/mcap-log.git
-git clone git@github.com:mwaylabs/mct-core.git
-symlink .
+git clone -b master git@github.com:mwaylabs/generator-m-server.git
+git clone -b master git@github.com:mwaylabs/generator-m.git
+git clone -b master git@github.com:mwaylabs/generator-mcap.git
+git clone -b master git@github.com:mwaylabs/mcap-application-validation.git
+git clone -b master git@github.com:mwaylabs/mcap-cli.git
+git clone -b master git@github.com:mwaylabs/mcaprc.git
+git clone -b master git@github.com:mwaylabs/mcap-deploy.git
+git clone -b master git@github.com:mwaylabs/mcap-log.git
+git clone -b master git@github.com:mwaylabs/mcap-logger.git
+git clone -b master git@github.com:mwaylabs/mct-core.git
+
+for d in ./*
+do
+  echo "Run npm install in" $d
+  cd $d && npm install
+  cd ../
+done
+
+# Link mcap command
+cd mcap-cli && npm link && cd ../
+
+# Create symlinks
+sh ./symlink.sh
---
+++ symlink.sh
@@ -0,0 +1,31 @@
+
+# Remove dep.
+rm -rf ./generator-m-server/node_modules/mcap-log
+rm -rf ./generator-mcap/node_modules/mcap-log
+rm -rf ./mcap-application-validation/node_modules/mcap-log
+rm -rf ./mcap-cli/node_modules/mcap-log
+rm -rf ./mcap-cli/node_modules/mcaprc
+rm -rf ./mcap-cli/node_modules/mct-core
+rm -rf ./mcap-deploy/node_modules/mcap-application-validation
+rm -rf ./mcap-deploy/node_modules/mcap-log
+rm -rf ./mct-core/node_modules/generator-m
+rm -rf ./mct-core/node_modules/generator-m-server
+rm -rf ./mct-core/node_modules/generator-mcap
+rm -rf ./mct-core/node_modules/mcap-deploy
+rm -rf ./mct-core/node_modules/mcap-log
+rm -rf ./mct-core/node_modules/mcap-logger
+
+# Create dep. symlinks
+ln -s ../../mcap-log ./generator-m-server/node_modules/mcap-log
+ln -s ../../mcap-log ./generator-mcap/node_modules/mcap-log
+ln -s ../../mct-core ./mcap-cli/node_modules/mct-core
+ln -s ../../mcaprc ./mcap-cli/node_modules/mcaprc
+ln -s ../../mcap-log ./mcap-cli/node_modules/mcap-log
+ln -s ../../mcap-application-validation ./mcap-deploy/node_modules/mcap-application-validation
+ln -s ../../mcap-log ./mcap-application-validation/node_modules/mcap-log
+ln -s ../../generator-m ./mct-core/node_modules/generator-m
+ln -s ../../generator-m-server ./mct-core/node_modules/generator-m-server
+ln -s ../../generator-mcap ./mct-core/node_modules/generator-mcap
+ln -s ../../mcap-deploy ./mct-core/node_modules/mcap-deploy
+ln -s ../../mcap-log ./mct-core/node_modules/mcap-log
+ln -s ../../mcap-logger ./mct-core/node_modules/mcap-logger
